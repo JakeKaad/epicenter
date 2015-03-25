ec14092a6163adecc3cc0b6e5cc7b1d7772b1471
Jake Kaad <kaadalac@gmail.com>
2015-03-25 11:49:41 -0700

    add validation specs and cohort association

--- app/models/internship.rb
+++ app/models/internship.rb
@@ -1,4 +1,6 @@
 class Internship < ActiveRecord::Base
   belongs_to :company
+  belongs_to :cohort
+  validates_presence_of :description, :ideal_intern
 
 end
--- spec/models/internship_spec.rb
+++ spec/models/internship_spec.rb
@@ -1,4 +1,8 @@
 describe Internship do
   it { should belong_to :company }
   it { should belong_to :cohort }
+  it { should validate_presence_of :description }
+  it { should validate_presence_of :ideal_intern }
+
+
 end
